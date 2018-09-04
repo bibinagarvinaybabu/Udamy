82f6cb89e1ade84ebe6b967bb51fc21139d6302c
VinayOfc <bibinagarvinaybabu@gmail.com>
2018-09-04 16:55:16 +0530

    added new class salesforce

---
+++ src/salesForce.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class salesForce {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse IDE 4.3\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();		
+		options.addArguments("--disable-extensions");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.get("http://facebook.com");
+		driver.findElement(By.id("email")).sendKeys("vinay210292@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("iloveyouvinay");
+		
+		
+		
+		
+		
+		
+		//driver.close();
+		quitmethod(driver);
+	}
+
+	public static void quitmethod(WebDriver driver) throws InterruptedException{
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
